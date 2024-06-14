e1a3c000d72fcae4e4da6b4e772004073826f04c
Ritiks289 <92520523+Ritiks289@users.noreply.github.com>
2024-06-14 11:07:15 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,32 @@
+"use strict"  //used for newer version of JS
+//alert wont work the same here because were using nodeJS here not browser
+
+let name="Ritik"
+let age=" 18"
+let isLoggedin="false"
+let state;
+
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Hitesh")
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
