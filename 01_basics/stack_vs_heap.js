901b629f1ec9640ab6bb4a54960ec6ecb93d431a
Ritiks289 <92520523+Ritiks289@users.noreply.github.com>
2024-07-03 07:43:15 +0000

    stack&heap

---
+++ 01_basics/stack_vs_heap.js
@@ -0,0 +1,20 @@
+// Stack(primitive_, Heap(Non-Premitive)
+
+let myyoutubename="ritikssingh"
+let anothername=myyoutubename
+anothername="lappupurkaraja"
+
+console.log(myyoutubename);
+console.log(anothername);
+
+let userone={
+    email:"user@gmail.com",
+    upi: "user@ybl"
+}
+
+let usertwo=userone
+
+usertwo.email="hiresohi@gmail.com"
+
+console.log(userone.email);
+console.log(usertwo.email);
