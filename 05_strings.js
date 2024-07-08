9bd82d8e245943666861681e865031d5c1eea187
Ritiks289 <92520523+Ritiks289@users.noreply.github.com>
2024-07-08 10:17:05 +0530

    Delete 05_strings.js

--- 05_strings.js
+++
@@ -1,9 +0,0 @@
-const name = "ritik"
-const reposcount="5"
-
-console.log(name+repocount+"Value");
-
-console.log(`Hello my name is ${name} and my repocount is ${repocount}`);
-
-const gamename = new String('hiteshhc') 
-console.log(gamename.toUpperCase())
