2562df770404ab97bf8b2db872056c0428358faa
Ritiks289 <92520523+Ritiks289@users.noreply.github.com>
2024-07-08 04:45:12 +0000

    Strings

---
+++ 05_strings.js
@@ -0,0 +1,9 @@
+const name = "ritik"
+const reposcount="5"
+
+console.log(name+repocount+"Value");
+
+console.log(`Hello my name is ${name} and my repocount is ${repocount}`);
+
+const gamename = new String('hiteshhc') 
+console.log(gamename.toUpperCase())
